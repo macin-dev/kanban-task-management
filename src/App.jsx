b9e6d6673a1001c449b2d4e224f1fcb63761ff41
macin-dev <ernesto.dev24@gmail.com>
2026-02-18 12:54:37 -0600

    feat: add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import Header from "./components/Header";
+
 export default function App() {
-  return <h1 className="text-2xl">Hello React!</h1>;
+  return (
+    <section>
+      <Header />
+    </section>
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { HiDotsVertical, HiPlus } from "react-icons/hi";
+import { MdKeyboardArrowUp } from "react-icons/md";
+
+export default function Header() {
+  return (
+    <header className="flex items-center gap-4 bg-white p-4">
+      <img src="/src/assets/logo.svg" alt="" />
+
+      <div className="inline-flex shrink-0 items-center gap-1.5">
+        <span className="inline-block text-lg font-bold">Platform Launch</span>
+
+        <div className="inline-block">
+          <MdKeyboardArrowUp />
+        </div>
+      </div>
+
+      <div className="ml-auto inline-flex items-center gap-4">
+        <div className="bg-primary rounded-3xl px-4 py-2 text-white">
+          <HiPlus />
+        </div>
+        <HiDotsVertical />
+      </div>
+    </header>
+  );
+}
